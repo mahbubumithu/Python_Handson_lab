4db3670bd9f4af62b549bb86602aa30d926bbaa4
MD. MAHBUB UDDIN <mahbubumithu@gmail.com>
2024-10-05 23:00:28 +0600

    Rename one file and add another add two number program

---
+++ Basic_programming/add_2_input_numbers.py
@@ -0,0 +1,7 @@
+num1 = input("First Number: ")
+num2 = input("\nSecond Number: ")
+#Adding two numbers
+
+sum = float(num1) + float(num2)
+
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
