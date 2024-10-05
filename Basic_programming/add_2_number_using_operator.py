842c6c80c11199aebe64984ea3cf77596264b67a
MD. MAHBUB UDDIN <mahbubumithu@gmail.com>
2024-10-05 23:13:38 +0600

    add two number using operator method

---
+++ Basic_programming/add_2_number_using_operator.py
@@ -0,0 +1,7 @@
+num1 = input("First Number: ")
+num2 = input("\nSecond Number: ")
+#Adding two numbers
+import operator
+sum = operator.add(int(num1), int(num2))
+
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
