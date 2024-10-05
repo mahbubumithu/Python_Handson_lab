8c82eceaf68e6cebf195b66c756941319f23f64c
MD. MAHBUB UDDIN <mahbubumithu@gmail.com>
2024-10-05 23:07:13 +0600

    add sum function

---
+++ Basic_programming/function_for_add_number.py
@@ -0,0 +1,8 @@
+def add_two_number(a,b):
+    return a+b
+num1 = float(input("Input First Number: "))
+num2 = float(input("\nInput Second Number: "))
+
+sum = add_two_number(num1,num2)
+
+print("Sum of {0} and {1} is {2}" .format(int(num1), int(num2), int(sum)))
