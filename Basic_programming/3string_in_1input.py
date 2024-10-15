eebf14aec4ab3edea207ed343bbf6c0e2455cec5
Mahbub_ofc_laptop <mahbubumithu@gmail.com>
2024-10-15 20:49:50 +0600

    3 string in 1 input call

---
+++ Basic_programming/3string_in_1input.py
@@ -0,0 +1,7 @@
+# Accept three strings from one input call
+str1, str2, str3 = input("Enter three strings separated by spaces: ").split()
+
+# Print the strings to confirm
+print("String 1:", str1)
+print("String 2:", str2)
+print("String 3:", str3)
